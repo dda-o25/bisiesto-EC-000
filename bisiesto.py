e9d73e8adfc91781e5645ff10664874b674890af
EC-000 <eduardoc.castillo9814@gmail.com>
2025-09-18 23:03:29 -0600

    Update bisiesto.py

--- bisiesto.py
+++ bisiesto.py
@@ -1,15 +1,16 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+El propósito de este programa es que basado en un año dado, este te dira si es bisiesto o no, siguiendo unas reglas
+Eduardo Caleb Castillo Llanas 18/Sep/25
 """
 
 # Declaraciones
-CONSTANTE = valor
+#CONSTANTE = valor
 
 # Entradas
-entrada = input()
+año = int(input())
 
 # Proceso
-
-
-# Salidas
-print(salida)
+if (año % 4 == 0) or (año % 400 == 0):
+    print(año,"si es un año bisiesto")
+else:
+    print(año,"no es un año bisiesto")
